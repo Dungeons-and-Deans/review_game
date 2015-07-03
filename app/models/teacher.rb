9c1f6a5d77dc70476919c631248478afdb35fc8c
Anna Bedell <awbedell@gmail.com>
2015-07-03 14:37:14 -0400

    devise controllers

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -4,10 +4,6 @@ class Teacher < ActiveRecord::Base
   has_many :students, dependent: :destroy
   has_many :games, dependent: :destroy
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
