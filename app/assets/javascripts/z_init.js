3d2eb159fc6751cd50828aa33032a7dcc37fb0e5
lee allen <leeallen337@gmail.com>
2015-07-04 11:24:47 -0400

    made logout link look like a button and be positioned in the top right - also seemed to fix the active status of the classes for the navigation bar

--- app/assets/javascripts/z_init.js
+++ app/assets/javascripts/z_init.js
@@ -1,16 +1,20 @@
 $(function () {
   'use strict';
   
+  var path = $(location).attr('pathname');
+  
+  $('.nav').find('.active').removeClass('active');
+  
   if ($('.simple_form.new_teacher').length) {
     app.loginNoNav();
   }
   
-  
-  
-  $(".nav a").on("click", function(){
-    $(".nav").find(".active").removeClass("active");
-    $(this).parent().addClass("active");
-  });
-
+  if (path.length === 1 || (/games/.test(path))) {
+    $('.nav .home').addClass('active');
+  } else if (/students/.test(path)) {
+    $('.nav .roster').addClass('active');
+  } else if (/categories/.test(path)) {
+    $('.nav .categories').addClass('active');
+  }
   
 });
