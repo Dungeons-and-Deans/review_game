d85dc4d5d5187e9f41fb793b7c4beba391fb63a1
Anna Bedell <awbedell@gmail.com>
2015-07-06 07:10:28 -0400

    question difficulty can't be negative

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ActiveRecord::Base
   belongs_to :category
 
   validates :content, presence: true
-  validates :difficulty_level, presence: true
+  validates :difficulty_level, numericality: { greater_than: 0 }
   validates :category_id, presence: true
 
 end
