a1f8be485937fcccd9b236aa8d5a4a106931a4c4
Anna Bedell <awbedell@gmail.com>
2015-07-06 13:10:22 -0400

    all but one teacher gameplay test passes

--- test/controllers/student_gameplay_controller_test.rb
+++ test/controllers/student_gameplay_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class StudentGameplayControllerTest < ActionController::TestCase
+  setup do
+    @game = games(:one)
+  end
+
   test "should get login" do
     get :login
     assert_response :success
--- test/controllers/teacher_gameplay_controller_test.rb
+++ test/controllers/teacher_gameplay_controller_test.rb
@@ -1,18 +1,23 @@
 require 'test_helper'
 
 class TeacherGameplayControllerTest < ActionController::TestCase
+  setup do
+    sign_in teachers(:three)
+    @game_session = game_sessions(:one)
+  end
+
   test "should get home" do
-    get :home
+    get :home, id: @game_session.id
     assert_response :success
   end
 
   test "should get supplies" do
-    get :supplies
+    get :supplies, id: @game_session.id
     assert_response :success
   end
 
   test "should get competition" do
-    get :competition
+    get :competition, id: @game_session.id
     assert_response :success
   end
 
