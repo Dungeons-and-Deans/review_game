90a4771ee928d010c115654a99b901dc9fcdf493
Anna Bedell <awbedell@gmail.com>
2015-07-07 17:34:49 -0400

    new question form on show page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,10 @@
 class Category < ActiveRecord::Base
   has_many :questions
   has_many :category_game_session_assignments
-  has_many :game_sessions, through: :category_game_session_assignments 
+  has_many :game_sessions, through: :category_game_session_assignments
   belongs_to :teacher
 
-  accepts_nested_attributes_for :questions, reject_if: :all_blank
+  accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true
 
   validates :name, presence: true
   validates :teacher_id, presence: true
