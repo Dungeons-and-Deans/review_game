4fa7b2bc00dacd59e4fcf0f0e6e3bb0a8275c34a
Anna Bedell <awbedell@gmail.com>
2015-07-07 07:43:07 -0400

    students names only appear on list if they aren't already in a group

--- app/controllers/game_sessions_controller.rb
+++ app/controllers/game_sessions_controller.rb
@@ -20,7 +20,7 @@ class GameSessionsController < ApplicationController
 
   def groups
     @game_session.groups.each { |group| group.group_assignments.build }
-    @students = Student.where(teacher_id: current_teacher.id)
+    @students = Student.where(teacher_id: current_teacher.id).select{ |s| s.unassigned?(@game_session.id) }
   end
 
   def assign_groups
