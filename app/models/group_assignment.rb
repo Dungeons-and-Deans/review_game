e098b10b66719071527f962957051bcc48677f94
Anna Bedell <awbedell@gmail.com>
2015-07-07 14:18:16 -0400

    if a student is dragged to a new group, the first assignment record is deleted

--- app/models/group_assignment.rb
+++ app/models/group_assignment.rb
@@ -8,6 +8,11 @@ class GroupAssignment < ActiveRecord::Base
 
   def self.assign(group_id, student_id, game_session_id)
     possible_groups = Group.where(game_session_id: game_session_id).map{|g| g.id}
-
+    possible_groups.each do |g|
+      group = GroupAssignment.where(group_id: g).find_by_student_id(student_id)
+      group.delete if group
+    end
+    GroupAssignment.create(group_id: group_id, student_id: student_id)
   end
+
 end
