f58bda0be81b6b1d471c1e26b56c11b1970b8896
Anna Bedell <awbedell@gmail.com>
2015-07-07 14:09:38 -0400

    pulling

--- app/models/group_assignment.rb
+++ app/models/group_assignment.rb
@@ -5,4 +5,9 @@ class GroupAssignment < ActiveRecord::Base
   validates :student_id, presence: true
   validates :group_id, presence: true
 
+
+  def self.assign(group_id, student_id, game_session_id)
+    possible_groups = Group.where(game_session_id: game_session_id).map{|g| g.id}
+
+  end
 end
