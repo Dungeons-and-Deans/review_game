5e8fb80fbb0f51d140137b445459386ddf590212
JohnDGreiner <john.d.greiner@gmail.com>
2015-07-08 09:15:57 -0400

    Add group and session id to student gameplay params.

--- app/controllers/student_gameplay_controller.rb
+++ app/controllers/student_gameplay_controller.rb
@@ -3,8 +3,7 @@ class StudentGameplayController < ApplicationController
     if request.post?
       group = Group.find_by_password(params[:password]["password"])
       if group
-        session[:group_id] = group.id
-        redirect_to student_gameplay_home_path, notice: "Logged in."
+        redirect_to "/student_gameplay/#{group.game_session_id}/home/#{group.id}", notice: "Logged in."
       else
         flash.now[:notice] = "Incorrect Password"
       end
@@ -12,7 +11,7 @@ class StudentGameplayController < ApplicationController
   end
 
   def home
-    @group = Group.find_by_id(session[:group_id])
+    @group = Group.find_by_id(params[:group_id])
     @game_session = GameSession.find(@group.game_session_id)
     @map = Map.find_by_game_id(@game_session.game.id)
     @players = @game_session.group_assignments.where(group_id: @group.id)
@@ -20,7 +19,6 @@ class StudentGameplayController < ApplicationController
   end
 
   def logout
-    session[:group_id] = nil
     redirect_to student_gameplay_login_path, notice: "Successfully logged out."
   end
 
