d5828c90426717f5ea9a107e98ae9304b85e7eb0
Anna Bedell <awbedell@gmail.com>
2015-07-09 09:23:35 -0400

    icons get automatic cooridinates for beginning of game

--- app/models/group_assignment.rb
+++ app/models/group_assignment.rb
@@ -4,7 +4,7 @@ class GroupAssignment < ActiveRecord::Base
 
   validates :student_id, presence: true
   validates :group_id, presence: true
-
+# 14, 20, 6
 
   def self.assign(group_id, student_id, game_session_id)
     possible_groups = Group.where(game_session_id: game_session_id).map{|g| g.id}
@@ -12,7 +12,9 @@ class GroupAssignment < ActiveRecord::Base
       group = GroupAssignment.where(group_id: g).find_by_student_id(student_id)
       group.delete if group
     end
-    GroupAssignment.create(group_id: group_id, student_id: student_id)
+    num_of_members = Group.find(group_id).students.count + 0
+    x = ((possible_groups.index(group_id.to_i)) * 200) + (num_of_members * 30)
+    GroupAssignment.create(group_id: group_id, student_id: student_id, board_x: x, board_y: 1200)
   end
 
 end
