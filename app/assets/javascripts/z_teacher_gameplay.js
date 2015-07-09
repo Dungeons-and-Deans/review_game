2ba9db549b577a58f09e5f1c246e75691405fb39
lee allen <leeallen337@gmail.com>
2015-07-09 10:44:50 -0400

    attempting to fix student question display problem

--- app/assets/javascripts/z_teacher_gameplay.js
+++ app/assets/javascripts/z_teacher_gameplay.js
@@ -3,6 +3,10 @@
 
 app.teacherGameplay = {
   nextQuestion: function () {
+    var groupId = $('.current-group').attr('id');
+    
+    console.log(groupId);
+    
     $('#send-question').on('click', function () {
       dispatcher.trigger('movements.ask_question', channelNumber);
     });
