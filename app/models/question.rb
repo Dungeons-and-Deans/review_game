18133d3e0671b78dd1dd19346153937a599d26ca
Anna Bedell <awbedell@gmail.com>
2015-07-10 07:32:29 -0400

    questions have correct and incorrect columns

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,12 @@ class Question < ActiveRecord::Base
   validates :difficulty_level, numericality: { greater_than: 0 }
   validates :category_id, presence: true
 
+  def percent_correct
+    (self.right / (self.right + self.wrong)) * 100
+  end
+
+  def percent_incorrect
+    (self.wrong / (self.right + self.wrong)) * 100
+  end
+
 end
