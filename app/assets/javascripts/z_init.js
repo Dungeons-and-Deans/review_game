fc7c5629ff3583adc416cdc076ccc00a9be13d12
Anna Bedell <awbedell@gmail.com>
2015-07-10 08:10:28 -0400

    correct incorrect buttons only appear once question is sent to students

--- app/assets/javascripts/z_init.js
+++ app/assets/javascripts/z_init.js
@@ -1,6 +1,6 @@
 $(function () {
   'use strict';
-  
+
   var pathName = $(location).attr('pathname');
 
   // if (!($('.simple_form#new_teacher').length || $('.login-group').length || /student_gameplay/.test(pathName))) {
@@ -18,24 +18,29 @@ $(function () {
   if ($('.groups-students').length) {
     app.gameSession.dragDrop();
   }
-  
+
   if (/teacher_gameplay/.test(pathName)) {
     app.teacherGameplay.nextQuestion();
-    
+
     $('button.choose-winner').on('click', function () {
       $('#myModal').toggleClass('active');
     });
-    
+
     $('button.choose-winner-close').on('click', function () {
       $('#myModal').toggleClass('active');
     });
   }
-  
+
   if (/student_gameplay/.test(pathName)) {
     app.studentGameplay.statMenu();
-    
+
     $('.nav').html($('#nav-links-student').html());
   }
-  
+
+  $("#answer-buttons").hide();
+  $("#send-question").on('click', function () {
+    $("#answer-buttons").show();
+  });
+
 
 });
