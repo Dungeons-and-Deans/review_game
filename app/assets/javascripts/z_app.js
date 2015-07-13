bd45e3fa751c57983c6c6a2a49b8a2bda7a3da5d
lee allen <leeallen337@gmail.com>
2015-07-13 16:42:39 -0400

    cleaning up js

--- app/assets/javascripts/z_app.js
+++ app/assets/javascripts/z_app.js
@@ -1,15 +1,3 @@
 var app = {
   
-  navActive: function (pathname) {
-    $('.nav').find('.active').removeClass('active');
-    
-    if (pathname.length === 1 || (/game/.test(pathname))) {
-      $('.nav .home').addClass('active');
-    } else if (/students/.test(pathname)) {
-      $('.nav .roster').addClass('active');
-    } else if (/categories/.test(pathname)) {
-      $('.nav .categories').addClass('active');
-    }
-  }
-  
 };
