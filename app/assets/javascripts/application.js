c7b436a5ed95f6e82e4eaf6751964e04dbc3993e
JohnDGreiner <john.d.greiner@gmail.com>
2015-07-13 00:00:37 -0400

    Student gameplay student list update with websocket.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -89,11 +89,16 @@ channelStudentQuestion.bind('ask_competition_question', function (questionText) 
 
 });
 
+channelStudentQuestion.bind('group_list', function (groupList) {
+  $('#group-list').empty();
+  groupList.map(function (group) {
+    $('#group-list').append("<li>" + group + "</li>");
+  });
+});
+
 channelStudentQuestion.bind('update_supplies', function (supplies) {
-  console.log(supplies);
   $('#supply-list').empty();
   supplies.map(function (supply) {
     $('#supply-list').append('<li>' + supply.amount + ' ' + supply.name + '</li>');
-    console.log("li");
   });
 });
