a5736f29c8874cd3f7420b92c5d6abc293a98394
Anna Bedell <awbedell@gmail.com>
2015-07-21 18:10:50 -0400

    all tests pass

--- test/controllers/class_gameplay_controller_test.rb
+++ test/controllers/class_gameplay_controller_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class ClassGameplayControllerTest < ActionController::TestCase
   setup do
     @game_session = game_sessions(:one)
+    @group = groups(:one)
+    @game_session.update(turn_group_id: @group.id)
   end
 
   test "should get home" do
--- test/controllers/student_gameplay_controller_test.rb
+++ test/controllers/student_gameplay_controller_test.rb
@@ -4,6 +4,8 @@ class StudentGameplayControllerTest < ActionController::TestCase
   setup do
     @game = games(:one)
     @group = groups(:one)
+    @game_session = game_sessions(:one)
+    @game_session.update(turn_group_id: @group.id)
   end
 
   test "should get login" do
